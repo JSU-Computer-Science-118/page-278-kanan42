f7848441fcca732b1eda389f278fa6d05e9ba51d
kanan <116582983+kanan42@users.noreply.github.com>
2022-11-17 10:14:42 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,46 @@
-// write your code here
+
+//Hannah Coleman J00963674
+//11/15/2022
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+const string SENTINEL = "-1";
+int main() {
+	string name;
+	int numOfVolunteers;
+	int numOfBoxesSold;
+	int totalNumOfBoxesSold;
+	double costOfOneBox;
+
+
+	cout << fixed << showpoint << setprecision(2);
+
+	totalNumOfBoxesSold = 0;
+	numOfVolunteers = 0;
+
+	cin >> name;
+
+	while (name != SENTINEL)
+	{
+		cin >> numOfBoxesSold;
+		totalNumOfBoxesSold = totalNumOfBoxesSold + numOfBoxesSold;
+
+		numOfVolunteers++;
+		cin >> name;
+	}
+	cout << endl;
+	cin >> costOfOneBox;
+	cout << endl;
+	cout << "Total number of boxes sold: "
+		<< totalNumOfBoxesSold << endl;	
+	cout << "Total money made: $"
+		<< totalNumOfBoxesSold * costOfOneBox << endl;
+	if (numOfVolunteers != 0)
+		cout << "Average number of boxes sold by each person: "
+		<< totalNumOfBoxesSold / numOfVolunteers
+		<< endl;
+	else
+		cout << "Line 34:No input." << endl;
+	return 0;
+}
